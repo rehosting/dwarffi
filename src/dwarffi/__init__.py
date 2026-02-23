127b1617944ce33f7be1f13389902376644386ee
Luke Craig <lacraig3@gmail.com>
2026-02-23 17:08:26 -0500

    dynamic versioning

--- src/dwarffi/__init__.py
+++ src/dwarffi/__init__.py
@@ -1,7 +1,9 @@
 """dwarffi package."""
+try:
+    from ._version import version as __version__
+except Exception:
+    __version__ = "0+unknown"
 
 __all__ = ["__version__"]
-
-__version__ = "0.1.0"
 from .dffi import DFFI
 from .core import *
