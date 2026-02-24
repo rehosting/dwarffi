f754867fc902df7000a0c08343dd8cbb447e6224
Luke Craig <lacraig3@gmail.com>
2026-02-24 13:59:11 -0500

    drop core facade

--- src/dwarffi/core.py
+++
@@ -1,7 +0,0 @@
-"""
-Facade for dwarffi.core to maintain backwards compatibility.
-"""
-
-from .instances import *  # noqa: F403
-from .parser import *  # noqa: F403
-from .types import *  # noqa: F403
